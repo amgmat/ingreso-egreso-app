8dc274f34a6b3d4caac82dcc8093eb7582de61b2
Ale <amg_29-90@hotmail.com>
2020-03-24 11:19:07 -0600

    Primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboradComponent } from './dashborad/dashborad.component';
+import { dashboradRoutes } from './dashborad/dashbord.routes';
+
+const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: '', component: DashboradComponent,
+        children:dashboradRoutes},
+    {path: '**', redirectTo: ''}
+]
+
+@NgModule({
+
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+
+})
+
+export class AppRoutingModule {}
